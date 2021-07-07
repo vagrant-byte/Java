c969de764fe3125fa5855c005eb9c7e5ac64936a
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-07-07 22:14:13 +0800

    数据结构

--- test.java
+++ test.java
@@ -177,6 +177,28 @@ public class test {
         }
         return list;
     }
+    //有效的数独
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row=new boolean[9][9];
+        boolean[][] col=new boolean[9][9];
+        boolean[][] block=new boolean[9][9];
+        for (int i = 0; i <9 ; i++) {
+            for (int j = 0; j <9 ; j++) {
+                if(board[i][j]!='.') {
+                    int num=board[i][j]-'1';
+                    int blockIndex=i/3*3+j/3;
+                    if(row[i][num]||col[j][num]||block[blockIndex][num]) {
+                        return false;
+                    } else {
+                        row[i][num]=true;
+                        col[j][num]=true;
+                        block[blockIndex][num]=true;
+                    }
+                }
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         int[] arr1=new int[] {7,4,5,3,6,1};
         System.out.println(maxProfit(arr1));

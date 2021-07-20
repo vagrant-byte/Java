124a21514f9fad555b0a007e819eaf28ebae1e49
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-07-20 19:29:30 +0800

    二叉树相关习题

--- Traversal.java
+++ Traversal.java
@@ -1,3 +1,5 @@
+import org.omg.PortableInterceptor.INACTIVE;
+
 import java.util.*;
 
 class TreeNode {
@@ -30,7 +32,7 @@ public class Traversal {
         E.right=H;
         return A;
     }
-    // 前序遍历
+    // 先序遍历
     void preOrderTraversal(TreeNode root){
         List<Integer> list=new ArrayList<>();
         if (root==null) {
@@ -192,6 +194,24 @@ public class Traversal {
         return isBalanced(root.left)&&isBalanced(root.right);
 
     }
+    //平衡二叉树 时间复杂度为o(n)
+    public int hight(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int leftDepth=hight(root.left);
+        int rightDepth=hight(root.right);
+        if(leftDepth>=0&&rightDepth>=0&&Math.abs(leftDepth-rightDepth)<=1) {
+            return Math.max(leftDepth,rightDepth)+1;
+        }else {
+            return -1;
+        }
+
+    }
+    public boolean isBalanced1(TreeNode root) {
+        return hight(root)>=0;
+
+    }
     //对称二叉树
     public boolean isSymmetric(TreeNode root) {
         if(root==null) {
@@ -290,15 +310,281 @@ public class Traversal {
         //整个树遍历完，是完全二叉树
         return true;
     }
+    //镜像对称二叉树
+    public boolean isSymmetricChild(TreeNode leftTree,TreeNode rightTree) {
+        if(leftTree==null||rightTree==null) {
+            return false;
+        }
+        if(leftTree.val!=rightTree.val) {
+            return false;
+        }
+        if(leftTree==null&&rightTree==null){
+            return true;
+        }
+        return isSymmetricChild(leftTree.left,leftTree.right)&&isSymmetricChild(leftTree.right,leftTree.left);
+    }
+    public boolean isSymmetric1(TreeNode root) {
+        if(root == null) {
+            return true;
+        }
+        return isSymmetricChild(root.left,root.right);
+    }
+    //二叉树的镜像
+    public TreeNode Mirror (TreeNode pRoot) {
+        // write code here
+        if(pRoot==null) {
+            return pRoot;
+        }
+        if(pRoot.right==null&&pRoot.left==null){
+            return pRoot;
+        }
+        TreeNode tmp=pRoot.left;
+        pRoot.left=pRoot.right;
+        pRoot.right=tmp;
+        if(pRoot.left!=null) {
+            Mirror(pRoot.left);
+        }
+        if(pRoot.right!=null) {
+            Mirror(pRoot.right);
+        }
+        return pRoot;
 
+    }
+    //层序遍历
+    void levelOrderTraversal1(TreeNode root) {
+        if(root==null) {
+            return;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        //循环
+        while (!queue.isEmpty()) {
+            TreeNode cur=queue.poll();
+            System.out.println(cur.val+" ");
+            if(root.left!=null) {
+                queue.offer(root.left);
+            }
+            if(root.right!=null) {
+                queue.offer(root.right);
+            }
+        }
+    }
+    // 判断一棵树是不是完全二叉树
+    boolean isCompleteTree1(TreeNode root) {
+        if(root==null) {
+            return true;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode cur=queue.poll();
+            if(cur==null) {
+                return false;
+            }
+            int size=queue.size();
+            while (size!=0) {
+                if(root.left!=null) {
+                    queue.offer(root.left);
+                }
+                if(root.right!=null) {
+                    queue.offer(root.right);
+                }
+                size--;
+            }
+        }
+        return true;
+    }
+    //非递归二叉树前序遍历
+    void preOrderTraversal1(TreeNode root) {
+        if(root==null) {
+            return;
+        }
+        TreeNode cur=root;
+        Stack<TreeNode> stack=new Stack<>();
+        while (cur!=null||!stack.empty()) {
+            while (cur!=null) {
+                stack.push(cur);
+                System.out.print(cur.val+" ");
+                cur=cur.left;
+            }
+            TreeNode top=stack.pop();
+            cur=top.right;
+        }
+    }
+    // 中序遍历
+    void inOrderTraversal1(TreeNode root) {
+        if(root==null) {
+            return;
+        }
+        TreeNode cur=root;
+        Stack<TreeNode> stack=new Stack<>();
+        while (cur!=null||!stack.empty()) {
+            while (cur!=null) {
+                stack.push(cur);
+                cur=cur.left;
+            }
+            TreeNode top=stack.pop();
+            System.out.print(top.val+" ");
+            cur=top.right;
+        }
+
+    }
+    // 后序遍历
+    void postOrderTraversal1(TreeNode root) {
+        if(root == null) return;
+        TreeNode cur = root;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;//用来指定 上一个被打印的元素
+
+        while (cur != null || !stack.empty()) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.peek();
+            if (cur.right == null || cur.right == pre) {
+                TreeNode popNode = stack.pop();
+                System.out.print(popNode.val + " ");
+                pre = cur;
+                cur = null;
+            } else {
+                cur = cur.right;
+            }
+        }
+        System.out.println();
+    }
+    // 遍历思路-求叶子结点个数
+    static int leafSize = 0;
+    void getLeafSize1(TreeNode root){
+        if(root==null) {
+            return;
+        }
+        if(root.left==null&&root.right==null) {
+            leafSize++;
+        }
+        getLeafSize1(root.left);
+        getLeafSize1(root.right);
+    }
+    // 子问题思路-求叶子结点个数
+    int getLeafSize2(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        if(root.left==null&&root.right==null) {
+            return 1;
+        }
+        return getLeafSize2(root.left)+getLeafSize2(root.right);
+
+    }
+    // 获取二叉树的高度
+    int getHeight(TreeNode root) {
+        if(root==null) {
+            return 0;
+        }
+        int LeftHeight=getHeight(root.left);
+        int RightHeight=getHeight(root.right);
+        return 1+(LeftHeight>RightHeight?LeftHeight:RightHeight);
+    }
+    // 查找 val 所在结点，没有找到返回 null
+// 按照 根 -> 左子树 -> 右子树的顺序进行查找
+// 一旦找到，立即返回，不需要继续在其他位置查找
+    TreeNode find1(TreeNode root, char val) {
+        if(root==null) {
+            return null;
+        }
+        if(root.val==val) {
+            return root;
+        }
+        TreeNode ret=find1(root.left,val);
+        if(ret!=null) {
+            return ret;
+        }
+        ret=find1(root.right,val);
+        if(ret!=null) {
+            return ret;
+        }
+        return null;
+    }
+    //两个相同的树
+    public boolean isSameTree1(TreeNode p, TreeNode q) {
+        if(p==null&&q==null) {
+            return true;
+        }
+        if(p==null||q==null) {
+            return false;
+        }
+        if(p.val!=q.val) {
+            return false;
+        }
+        return isSameTree1(p.left,q.left)&&isSameTree1(p.right,q.right);
+    }
+    //判断子树
+    public boolean isSubtree1(TreeNode root, TreeNode subRoot) {
+        if(root==null||subRoot==null) {
+            return false;
+        }
+        if(isSameTree1(root,subRoot)) {
+            return true;
+        }
+        if(isSubtree1(root.left,subRoot)) {
+            return true;
+        }
+        if(isSubtree1(root.right,subRoot)) {
+            return true;
+        }
+        return false;
+    }
+    //递增顺序搜索树
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Character> res=new ArrayList<>();
+        inorder(root,res);
+        TreeNode top=new TreeNode('Q');
+        TreeNode cur=top;
+        for (char val:res) {
+            cur.right=new TreeNode(val);
+            cur=cur.right;
+        }
+        return top.right;
+    }
+    public void inorder(TreeNode node, List<Character> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+    //合并两个二叉树
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1==null&&root2==null) {
+            return null;
+        }
+        if(root1==null) {
+            return root2;
+        }
+        if(root2==null) {
+            return root1;
+        }
+        root1.val+=root2.val;
+        root1.left=mergeTrees(root1.left,root2.left);
+        root1.right=mergeTrees(root1.right,root2.right);
+        return root1;
+    }
     public static void main(String[] args) {
         Traversal traversal=new Traversal();
         TreeNode root=traversal.createTree();
         traversal.preOrderTraversal(root);
         System.out.println();
+        traversal.preOrderTraversal1(root);
+        System.out.println();
         traversal.inOrderTraversal(root);
         System.out.println();
+        traversal.inOrderTraversal1(root);
+        System.out.println();
         traversal.postOrderTraversal(root);
+        System.out.println();
+        traversal.postOrderTraversal1(root);
         System.out.println();
         System.out.println(traversal.getSize2(root));
         int r=traversal.getLeafSize(root);

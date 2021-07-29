3aaf42a58560384c8ab0289559ce2d3565fc5bc5
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-07-29 19:20:59 +0800

    与map和set相关习题

--- MapSet.java
+++ MapSet.java
@@ -1,6 +1,151 @@
 import java.util.*;
 
 public class MapSet {
+    //前k个高频单词
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> list=new ArrayList<>();
+        //1.统计每次单词出现的频率
+        HashMap<String,Integer> map=new HashMap<>();
+        for (String s:words) {
+            if(map.get(s)==null) {
+                map.put(s,1);
+            } else {
+                int count=map.get(s);
+                map.put(s,count+1);
+            }
+        }
+        //2.创建一个小堆
+        PriorityQueue<Map.Entry<String,Integer>> minHeap=new PriorityQueue<>(k, new Comparator<Map.Entry<String, Integer>>() {
+            @Override
+            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                if(o1.getValue()==o2.getValue()) {
+                    return o2.getKey().compareTo(o1.getKey());
+                }
+                return o1.getValue()-o2.getValue();
+            }
+        });
+        //遍历map 将前K个先放入小堆 然后从第K+1个开始 和堆顶元素比较
+        for (Map.Entry<String,Integer> entry:map.entrySet()) {
+            if(minHeap.size()<k) {
+                minHeap.offer(entry);
+            } else {
+                Map.Entry<String,Integer> tmp=minHeap.peek();
+                if(tmp!=null) {
+                    //频率相同 判断字母
+                    if(tmp.getValue()==entry.getValue()) {
+                        if(tmp.getKey().compareTo(entry.getKey())>0) {
+                            minHeap.poll();
+                            minHeap.offer(entry);
+                        }
+                    } else {
+                        if(tmp.getValue()<entry.getValue()) {
+                            minHeap.poll();
+                            minHeap.offer(entry);
+                        }
+                    }
+                }
+            }
+        }
+        for (int i = 0; i <k ; i++) {
+            Map.Entry<String,Integer> top = minHeap.poll();
+            if(top != null) {
+                String key = top.getKey();
+                list.add(key);
+            }
+        }
+        Collections.reverse(list);
+        return list;
+
+    }
+    //最常见的单词
+    public String mostCommonWord(String paragraph, String[] banned) {
+        String[] par=paragraph.split("[,.;!?]");
+        HashMap<String,Integer> map=new HashMap<>();
+        String ans=null;
+        int max=0;
+        for (int i=0;i<par.length;i++) {
+            boolean flag=false;
+            for (int j=0;j<banned.length;j++) {
+                if(par[i].equals(banned[j])) {
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag) {
+                map.put(par[i],map.getOrDefault(par[i],0)+1);
+                if(!par[i].equals("")&&map.get(par[i])>max) {
+                    max=map.get(par[i]);
+                    ans=par[i];
+                }
+            }
+        }
+        return ans;
+    }
+    //字符串中的第一个唯一字符
+    public int firstUniqChar(String s) {
+        if(s==null) {
+            return -1;
+        }
+        HashMap<Character,Integer> map=new HashMap<>();
+        for (char ch:s.toCharArray()) {
+            if(map.get(ch)==null) {
+                map.put(ch,1);
+            } else {
+                int count=map.get(ch);
+                map.put(ch,count+1);
+            }
+        }
+        for (int i = 0; i <s.length() ; i++) {
+            if(map.get(s.charAt(i))==1) {
+                return i;
+            }
+        }
+        return -1;
+
+    }
+    //两个数的交集
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null) {
+            return null;
+        }
+        HashSet<Integer> set=new HashSet<>();
+        for (int i = 0; i <nums1.length ; i++) {
+            set.add(nums1[i]);
+        }
+        HashSet<Integer> set1=new HashSet<>();
+        for (int i = 0; i <nums2.length ; i++) {
+            if(set.contains(nums2[i])) {
+                set1.add(nums2[i]);
+            }
+        }
+        int[] array=new int[set1.size()];
+        int count=0;
+        for (int i:set1) {
+            array[count]=i;
+            count++;
+        }
+        return array;
+    }
+    //同构字符串
+    public boolean isIsomorphic(String s, String t) {
+        if(s.length()!=t.length()) {
+            return false;
+        }
+        HashMap<Character,Character> map=new HashMap<>();
+        for (int i = 0; i <s.length() ; i++) {
+            if(!map.containsKey(s.charAt(i))) {
+                if(map.containsValue(t.charAt(i))) {
+                    return false;
+                }
+                map.put(s.charAt(i),t.charAt(i));
+            } else {
+                if(map.get(s.charAt(i))!=t.charAt(i)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
     //只出现一次的数字
     public int singleNumber(int[] nums) {
         HashSet<Integer> set=new HashSet<>();
@@ -55,7 +200,6 @@ public class MapSet {
                 System.out.println(ch);
             }
         }
-
     }
     //宝石与石头
     public int numJewelsInStones(String jewels, String stones) {

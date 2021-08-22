5f572bdd1d0aa350fd8e027d4a4d8bab835bc091
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-08-22 22:26:45 +0800

    剑指offer练习

--- exercise.java
+++ exercise.java
@@ -1,7 +1,51 @@
 package 剑指offer;
 
 import java.util.Stack;
+//复杂链表的复制
+/*class Node {
+    int val;
+    Node next;
+    Node random;
 
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+    public Node copyRandomList(Node head) {
+        if(head==null) {
+            return null;
+        }
+        Node cur=head;
+        //链表节点复制
+        while(cur!=null) {
+            Node copyNode=new Node(cur.val);
+            copyNode.next=cur.next;
+            cur.next=copyNode;
+            cur=cur.next.next;
+        }
+        //从头开始遍历链表，通过 cur.next.random = cur.random.next 可以将复制节点的随机指针串起来，当然需要判断 cur.random 是否存在
+        cur=head;
+        while(cur!=null) {
+            if(cur.random!=null) {
+                cur.next.random=cur.random.next;
+            }
+            cur=cur.next.next;
+        }
+        //将链表一分为2
+        Node copyHead=head.next;
+        cur = head;
+        Node curCopy = head.next;
+        while (cur != null) {
+            cur.next = cur.next.next;
+            cur = cur.next;
+            if (curCopy.next != null) {
+                curCopy.next = curCopy.next.next;
+                curCopy = curCopy.next;
+            }
+        }
+        return copyHead;
+    }*/
 public class exercise {
     Stack<Integer> stack=new Stack<>();
     Stack<Integer> minStack=new Stack<>();

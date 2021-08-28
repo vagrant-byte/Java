de680ebef5ad9bd5cb3f1b0d7581504f72a07014
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-08-28 23:10:50 +0800

    剑指offer

--- exercise.java
+++ exercise.java
@@ -158,4 +158,33 @@ public class exercise {
         }
         return ' ';
     }
+    //斐波那契数列 青蛙跳台阶
+    public int numWays(int n) {
+        if(n<2) {
+            return 1;
+        }
+        int [] num=new int[n+1];
+        num[0]=1;
+        num[1]=1;
+        for(int i=2;i<=n;i++) {
+            num[i]=(num[i-1]+num[i-2])%1000000007;
+        }
+        return num[n];
+    }
+    //股票最大利润
+    public int maxProfit(int[] prices) {
+        if(prices==null||prices.length<=1) {
+            return 0;
+        }
+        int res=0;
+        int min=prices[0];
+        for (int i = 1; i <prices.length ; i++) {
+            if(prices[i]<min) {
+                min=prices[i];
+            } else {
+                res=Math.max(res,prices[i]-min);
+            }
+        }
+        return res;
+    }
 }

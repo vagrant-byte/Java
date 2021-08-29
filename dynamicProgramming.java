4f88d6326de1df2598962e94b604c0d8d3711df6
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-08-29 22:29:40 +0800

    动态规划

--- dynamicProgramming.java
+++ dynamicProgramming.java
@@ -30,4 +30,31 @@ public class dynamicProgramming {
         }
         return res;
     }
+    //连续子数组最大和
+    public int maxSubArray(int[] nums) {
+        int sum=0;
+        int max=Integer.MIN_VALUE;
+        for (int i = 0; i <nums.length ; i++) {
+            if(sum<=0) {
+                sum=nums[i];
+            } else {
+                sum+=nums[i];
+            }
+            max=Math.max(sum,max);
+        }
+        return max;
+    }
+    //礼物的最大价值
+    public int maxValue(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int []dp = new int[n];
+        for(int i = 0; i < m; i++){
+            dp[0] = dp[0] + grid[i][0];
+            for(int j = 1; j < n; j++){
+                dp[j] = Math.max(dp[j], dp[j-1]) + grid[i][j];
+            }
+        }
+        return dp[n-1];
+    }
 }

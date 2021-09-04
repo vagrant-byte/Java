5565ab6df6bcddf89dc5054d6a280231d313d2c0
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-04 17:23:11 +0800

    查找算法

--- lookupAlgorithm.java
+++ lookupAlgorithm.java
@@ -81,6 +81,42 @@ public class lookupAlgorithm {
                 return numbers[i];
         return numbers[0];
     }
+    //旋转数组的最小数
+    public int minNumberInRotateArray(int [] array) {
+        if(array.length==0||array==null) {
+            return 0;
+        }
+        //中间值查找 数组分为两部分两部分都有序 如果中间值大于left下标值证明left到mid有序最小的在右半部分反之在左半部分
+        int left=0;
+        int right=array.length-1;
+        int mid=0;
+        while (array[left]>=array[right]) {
+            //左右相邻最小的为最右边因为数组有序
+            if(right-left==1) {
+                mid=right;
+                break;
+            }
+            mid=(left+right)/2;
+            //当三个数都相等时线性遍历
+            if(array[left]==array[mid]&&array[mid]==array[right]) {
+                int result=array[left];
+                for (int i = left+1; i <right ; i++) {
+                    if(result>array[i]) {
+                        result=array[i];
+                    }
+                }
+                return result;
+            }
+
+            if(array[mid]>=array[left]) {
+                left=mid;
+            } else {
+                right=mid;
+            }
+        }
+        return array[mid];
+
+    }
     //第一次只出现一次的字符
     public char firstUniqChar(String s) {
         int[] count = new int[256];
@@ -92,5 +128,81 @@ public class lookupAlgorithm {
                 return c;
         }
         return ' ';
+    }
+    //奇数在前偶数在后顺序不变
+    public void reOrderArray(int [] array) {
+        if(array==null||array.length==0) {
+            return;
+        }
+        //保持顺序不变采用直接插入的方法 找到奇数将其保存后，将其前面的数字向后移插入奇数
+        int k=0;//记录奇数插入位置
+        for (int i = 0; i <array.length ; i++) {
+            //表示奇数
+            if((array[i]&1)==1) {
+                int tmp=array[i];
+                int j=i;
+                //将要插入位置到找到奇数位置间的元素都往后移
+                while (j>k) {
+                    array[j]=array[j-1];
+                    j--;
+                }
+                array[k]=tmp;
+                k++;
+            }
+        }
+    }
+    //数组中出现次数一半以上的数字
+    public int MoreThanHalfNum_Solution(int [] array) {
+        if(array==null||array.length==0) {
+            return 0;
+        }
+        int number=array[0];
+        int times=1;
+        for (int i = 1; i <array.length ; i++) {
+            if(times==0) {
+                number=array[i];
+                times=1;
+            }else if(array[i]==number) {
+                times++;
+            } else {
+                times--;
+            }
+        }
+        int count=0;
+        for (int i = 0; i <array.length ; i++) {
+            if(array[i]==number) {
+                count++;
+            }
+        }
+        if(count>=array.length/2) {
+            return number;
+        }
+        return 0;
+    }
+    //替换空格
+    public String replaceSpace(StringBuffer str) {
+        int count=0;//用来记录空格个数
+        for (int i = 0; i <str.length() ; i++) {
+            if (str.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        int new_length=str.length()+2*count;
+        int old_end=str.length()-1;
+        int new_end=new_length-1;
+        str.setLength(new_length);
+        while (old_end>=0&&new_end>=0) {
+            if(str.charAt(old_end)==' ') {
+                str.setCharAt(new_end--,'0');
+                str.setCharAt(new_end--,'2');
+                str.setCharAt(new_end--,'%');
+                old_end--;
+            } else {
+                str.setCharAt(new_end--,str.charAt(old_end));
+                old_end--;
+
+            }
+        }
+        return str.toString();
     }
 }

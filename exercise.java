289f0433cd59dacd569d025968cafb5030b5797a
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-13 17:23:19 +0800

    剑指offer

--- exercise.java
+++ exercise.java
@@ -426,4 +426,18 @@ public class exercise {
             B[i] *= product;
         return B;
     }
+    //返回圆圈里的最后一个数
+    public int lastRemaining(int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            list.add(i);
+        }
+        int idx = 0;
+        while (n > 1) {
+            idx = (idx + m - 1) % n;
+            list.remove(idx);
+            n--;
+        }
+        return list.get(0);
+    }
 }

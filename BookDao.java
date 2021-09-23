0a8ab9f128c971e44c834dcd45101af71b07de27
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-23 22:04:08 +0800

    book书籍的增删改查

--- BookDao.java
+++ BookDao.java
@@ -1,13 +1,11 @@
 package jdbc;
 
-import com.mysql.cj.MysqlConnection;
-import com.mysql.cj.jdbc.ha.MultiHostMySQLConnection;
-import jdbc.operation.DBUtil;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 //用来访问Book数据的对象
 public class BookDao {
@@ -41,95 +39,218 @@ public class BookDao {
         return false;
     }
     //2.查看所有书籍（查找)
-    public void search() {
+    public List<Book> search() {
         Connection connection=null;
         PreparedStatement statement=null;
         ResultSet resultSet=null;
+        List<Book> books=new ArrayList<>();
         try {
             //1.和数据库建立连接
             connection=DBUtil.getConnection();
             //2.拼装SQL语句
             String sql="select * from book";
-            statement=connection.prepareStatement(sql);
             //3.执行SQL语句
+            statement=connection.prepareStatement(sql);
             resultSet=statement.executeQuery();
             //4.遍历结果集
             while (resultSet.next()) {
-                int id=resultSet.getInt("id");
-                String name=resultSet.getString("name");
-                String author=resultSet.getString("author");
-                int price=resultSet.getInt("price");
-                String type=resultSet.getString("type");
-                int isBorrow=resultSet.getInt("isBorrow");
+                Book book=new Book();
+                book.setBookid(resultSet.getInt("id"));
+                book.setName(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setPrice(resultSet.getInt("price"));
+                book.setType(resultSet.getString("type"));
+                book.setBorrow(resultSet.getInt("isBorrow")==1);
+                books.add(book);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             DBUtil.close(connection,statement,resultSet);
         }
+        return books;
     }
     //3.根据书名找书籍
-    public int find(Book book) {
+    public List<Book> find(String name) {
         //查找book书籍
         Connection connection=null;
         PreparedStatement statement=null;
-        int ret=0;
+        ResultSet resultSet=null;
+        List<Book> books=new ArrayList<>();
         try {
             //与数据库建立连接
             connection=DBUtil.getConnection();
             //拼装SQL语句
-            String sql="select * from book where name=book.name";
+            String sql="select * from book where name =?";
             //执行SQL语句
-            ret=statement.executeUpdate(sql);
+            statement=connection.prepareStatement(sql);
+            statement.setString(1,name);
+            resultSet=statement.executeQuery();
+            while (resultSet.next()) {
+                Book book=new Book();
+                book.setBookid(resultSet.getInt("id"));
+                book.setName(resultSet.getString("name"));
+                book.setAuthor(resultSet.getString("author"));
+                book.setPrice(resultSet.getInt("price"));
+                book.setType(resultSet.getString("type"));
+                book.setBorrow(resultSet.getInt("isBorrow")==1);
+                books.add(book);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             DBUtil.close(connection,statement,null);
         }
-        return ret;
+        return books;
     }
     //4.删除书籍
-    public int del(Book book) {
-        Connection connection=null;
-        PreparedStatement statement=null;
-        int ret=0;
+    public boolean del(int bookid) {
+        Connection connection = null;
+        PreparedStatement statement = null;
         try {
-            connection=DBUtil.getConnection();
-            String sql="delete from book where name=book.name ";
-            ret=statement.executeUpdate(sql);
-        } catch (SQLException e) {
+            connection = DBUtil.getConnection();
+            String sql="delete from book where id=?";
+            statement=connection.prepareStatement(sql);
+            statement.setInt(1,bookid);
+            int ret=statement.executeUpdate();
+            if(ret!=1) {
+                return false;
+            }
+            return true;
+        }  catch (SQLException e) {
             e.printStackTrace();
         } finally {
             DBUtil.close(connection,statement,null);
         }
-        return ret;
+        return false;
     }
     //5.借书
-    //6.还书
-    public int Return () {
+    public boolean borrow(int bookid) {
         Connection connection=null;
-        PreparedStatement statement=null;
-        int ret=0;
+        PreparedStatement statement1=null;
+        PreparedStatement statement2=null;
+        ResultSet resultSet=null;
         try {
             connection=DBUtil.getConnection();
-            String name="围城";
-            String author="钱钟书";
-            int price=35;
-            String type="文学小说";
-            int isBorrow=1;
-            String sql="insert into book values(null,?,?,?,?,?)";
-            statement.setString(2,name);
-            statement.setString(3,author);
-            statement.setInt(4,price);
-            statement.setString(5,type);
-            statement.setInt(6,isBorrow);
-            ret=statement.executeUpdate();
+            String sql="select * from book where id=?";
+            statement1=connection.prepareStatement(sql);
+            statement1.setInt(1,bookid);
+            resultSet=statement1.executeQuery();
+            if(resultSet.next()) {
+                boolean isBorrow=(resultSet.getInt("isBorrow")==1);
+                if(isBorrow) {
+                    System.out.println("书已借出，无法借阅");
+                    return false;
+                }
+            } else {
+                System.out.println("书不存在，无法借阅");
+                return false;
+            }
+            sql="update book set isBorrow = 1 where id=?";
+            statement2=connection.prepareStatement(sql);
+            statement2.setInt(1,bookid);
+            int ret=statement2.executeUpdate();
+            if(ret!=1) {
+                return false;
+            }
+            return true;
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            DBUtil.close(connection,statement,null);
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement2 != null) {
+                try {
+                    statement2.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement1 != null) {
+                try {
+                    statement1.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        return ret;
+        return false;
+        }
+    //6.还书
+    public boolean Return(int bookid) {
+        Connection connection=null;
+        PreparedStatement statement1=null;
+        PreparedStatement statement2=null;
+        ResultSet resultSet=null;
+        try {
+            connection=DBUtil.getConnection();
+            String sql="select * from book where id=?";
+            statement1=connection.prepareStatement(sql);
+            statement1.setInt(1,bookid);
+            resultSet=statement1.executeQuery();
+            if(resultSet.next()) {
+                boolean isBorrow=(resultSet.getInt("isBorrow")==1);
+                if(!isBorrow) {
+                    System.out.println("书已存在，无法归还");
+                    return false;
+                }
+            } else {
+                System.out.println("书不存在，无法归还");
+                return false;
+            }
+            sql="update book set isBorrow = 0 where id=?";
+            statement2=connection.prepareStatement(sql);
+            statement2.setInt(1,bookid);
+            int ret=statement2.executeUpdate();
+            if(ret!=1) {
+                return false;
+            }
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement2 != null) {
+                try {
+                    statement2.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (statement1 != null) {
+                try {
+                    statement1.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return false;
     }
-
 }
+

eb143d8e2293aeea145b6b677a5817c1198f4428
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-09-28 21:21:32 +0800

    链表题

--- LinkedList.java
+++ LinkedList.java
@@ -132,5 +132,25 @@ public class LinkedList {
         }
         return as;
     }
+    //删除链表重复节点
+    public ListNode deleteDuplication(ListNode pHead) {
+        ListNode newHead=new ListNode();
+        ListNode tmp=newHead;
+        ListNode cur=pHead;
+        while (cur!=null) {
+            if(cur.next!=null&&cur.val==cur.next.val) {
+                while (cur.next!=null&&cur.val==cur.next.val) {
+                    cur=cur.next;
+                }
+                cur=cur.next;
+            } else {
+                tmp.next=cur;
+                cur=cur.next;
+                tmp=tmp.next;
+            }
+            tmp.next=null;
+        }
 
+        return newHead.next;
+    }
 }

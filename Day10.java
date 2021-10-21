69c4bbb73bb6de867b4e3fede4d49962d8721c20
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-10-21 17:21:18 +0800

    加一 密码等级 三子棋

--- Day10.java
+++ Day10.java
@@ -1,6 +1,22 @@
 import java.util.Scanner;
 
 public class Day10 {
+    //加一
+    public int[] plusOne(int[] digits) {
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                break;
+            }
+        }
+        int[] ret = new int[digits.length + 1];
+        if (digits[0] == 0) {
+            ret[0] = 1;
+        } else return digits;
+        return ret;
+    }
     //密码等级
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);

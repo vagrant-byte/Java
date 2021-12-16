a5dc561bd03111f7f25439cfc99256cc24be4ed8
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-12-16 22:43:56 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -1,5 +1,21 @@
 import java.util.*;
  class ListNode {
+     //盛最多水的容器
+     public int maxArea(int[] height) {
+         int left=0;
+         int right=height.length-1;
+         int ans=0;
+         while (left<right) {
+             int sum=Math.min(height[left],height[right])*(right-left);
+             ans=Math.max(ans,sum);
+             if(height[left]>height[right]) {
+                 right--;
+             } else {
+                 left++;
+             }
+         }
+         return ans;
+     }
      //旋转矩阵
      public void rotate(int[][] matrix) {
          int n=matrix.length;

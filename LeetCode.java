c23af098f488699ed89dce396539a241c99dcc13
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-12-29 20:38:17 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -2,6 +2,23 @@ import com.sun.javafx.image.impl.BaseByteToIntConverter;
 
 import java.util.*;
  class ListNode {
+     //统计特殊四元组
+     public int countQuadruplets(int[] nums) {
+         int count=0;
+         for (int a = 0; a <nums.length ; a++) {
+             for (int b =a+1 ; b <nums.length ; b++) {
+                 for (int c = b+1; c <nums.length ; c++) {
+                     for (int d = c+1; d <nums.length ; d++) {
+                         if(nums[a]+nums[b]+nums[c]==nums[d]) {
+                             count++;
+                         }
+                     }
+                 }
+             }
+         }
+         return count;
+
+     }
      //括号匹配问题
      public boolean chkParenthesis(String A, int n) {
          // write code here

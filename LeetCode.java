bb0f9dfffcf926b2e01f0fc847d535e93374c3f6
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2021-12-31 22:03:50 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -2,6 +2,19 @@ import com.sun.javafx.image.impl.BaseByteToIntConverter;
 
 import java.util.*;
  class ListNode {
+     //完美数
+     public boolean checkPerfectNumber(int num) {
+         int sum=0;
+         for(int i=1;i<num;i++) {
+             if(num%i==0) {
+                 sum+=i;
+             }
+             if(sum>num) {
+                 return false;
+             }
+         }
+         return num==sum;
+     }
      //一手顺子
      public boolean isNStraightHand(int[] hand, int groupSize) {
          if(hand.length%groupSize!=0) {

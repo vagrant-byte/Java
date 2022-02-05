c20d0266f32035bd271e61a8e5b6ff63cbe1c7a6
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-02-05 20:30:25 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -4,6 +4,27 @@ import java.io.CharArrayWriter;
 import java.security.KeyStore;
 import java.util.*;
  class ListNode {
+     //黄金矿工
+     int ans=0;
+     int grid[][];
+     public int getMaximumGold(int[][] grid) {
+         this.grid=grid;
+         for(int i=0;i<grid.length;i++){for(int j=0;j<grid[0].length;j++){if(grid[i][j]>0){getGold(i,j,0);}}}
+         return ans;
+     }
+     public void getGold(int i,int j,int gold){
+         if(i==-1||j==-1||i==grid.length||j==grid[0].length||grid[i][j]==0){return;}
+         int g=grid[i][j];
+         gold+=g;
+         grid[i][j]=0;
+         ans=Math.max(ans,gold);
+         getGold(i+1,j,gold);
+         getGold(i-1,j,gold);
+         getGold(i,j+1,gold);
+         getGold(i,j-1,gold);
+         gold-=g;
+         grid[i][j]=g;
+     }
      //可以形成最大正方形的矩形数目
      public static int countGoodRectangles(int[][] rectangles) {
          int maxlen=0;

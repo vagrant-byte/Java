f931fec1976558d1307dc7f310b8345ca5d25c4b
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-02-12 20:29:15 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -5,6 +5,40 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.util.*;
  class ListNode {
+     //飞地的数量
+     public int numEnclaves(int[][] grid) {
+         int m=grid.length;
+         int n=grid[0].length;
+         for(int i=0;i<m;i++) {
+             for (int j = 0; j <n ; j++) {
+                 //将其边界的值的上下左右为1的都置为0
+                 if((i==0||j==0||i==m-1||j==n-1)&&grid[i][j]==1) {
+                     dfs(grid,i,j);
+                 }
+             }
+         }
+         int count=0;
+         for (int i = 0; i <m ; i++) {
+             for (int j = 0; j <n ; j++) {
+                 if(grid[i][j]==1) {
+                     count++;
+                 }
+             }
+         }
+         return count;
+     }
+
+     private void dfs(int[][] grid, int i, int j) {
+         if(i<0||i>=grid.length||j<0||j>=grid[0].length||grid[i][j]==0) {
+             return;
+         }
+         grid[i][j]=0;
+         dfs(grid,i+1,j);
+         dfs(grid,i-1,j);
+         dfs(grid,i,j+1);
+         dfs(grid,i,j-1);
+     }
+
      //学生分数的最小差值
      public static int minimumDifference(int[] nums, int k) {
          if(k==1) {

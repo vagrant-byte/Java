d7d13fc03a89797d0223558546f0b25bc933d745
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-02-26 22:06:30 +0800

    每日一题

--- LeetCode.java
+++ LeetCode.java
@@ -5,6 +5,19 @@ import java.io.InputStream;
 import java.security.KeyStore;
 import java.util.*;
  class ListNode {
+     //增量元素之间的的最大差值
+     public int maximumDifference(int[] nums) {
+         int max=-1;
+         for (int i = 0; i <nums.length-1 ; i++) {
+             for (int j = i+1; j <nums.length ; j++) {
+                 if(nums[j]>nums[i]&&nums[j]-nums[i]>max) {
+                     max=nums[j]-nums[i];
+                 }
+             }
+         }
+         return max;
+
+     }
      //复数乘法
      public static String complexNumberMultiply(String num1, String num2) {
          int index1=0;

c2f8eb97f557e1c72295439d190a1f2ed60b010e
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-03-09 21:35:55 +0800

    三数之和盛最多水的容器

--- isvalid.java
+++ isvalid.java
@@ -1,7 +1,61 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Stack;
 
-//有效的括号
+
 public class isvalid {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (nums.length < 3) {
+            return res;
+        }
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int k = nums.length - 1;
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                while (k>j&&nums[j]+nums[k]>-nums[i]) {
+                    k--;
+                }
+                if(j==k) {
+                    break;
+                }
+                if(nums[i]+nums[j]+nums[k]==0){
+                    List<Integer> list1 = new ArrayList<>();
+                    list1.add(nums[i]);
+                    list1.add(nums[j]);
+                    list1.add(nums[k]);
+                    res.add(list1);
+                }
+            }
+        }
+        return res;
+    }
+
+        //盛最多水的容器
+    public int maxArea(int[] height) {
+        int left=0;
+        int right=height.length-1;
+        int ans=0;
+        while (left<right) {
+            int sum=Math.min(height[left],height[right])*(right-left);
+            ans=Math.max(ans,sum);
+            if(height[left]<height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return ans;
+    }
+    //有效的括号
     public static boolean isValid(String s) {
         if(s.length()==1) {
             return false;

727337eddaf177a90c0a18326540b4465061a0d4
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-03-15 22:09:52 +0800

    搜素旋转排列数组

---
+++ search.java
@@ -0,0 +1,28 @@
+public class search {
+    //搜素旋转排列数组
+    public int search(int[] nums, int target) {
+        int len=nums.length;
+        int left=0;
+        int right=len-1;
+        while (left<=right) {
+            int mid=(left+right)/2;
+            if(nums[mid]==target) {
+                return mid;
+            } else if(nums[mid]<nums[right]) {
+                if(nums[mid]<target&&target<=nums[right]) {
+                    left=mid+1;
+                }else {
+                    right=mid-1;
+                }
+            } else {
+                if(nums[left]<=target&&target<nums[mid]) {
+                    right=mid-1;
+                } else {
+                    left=mid+1;
+                }
+            }
+        }
+        return -1;
+
+    }
+}

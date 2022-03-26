ee5be82cd45b3220158073f2d8dbab8635241b47
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-03-26 21:04:07 +0800

    最小路径和

---
+++ minPathSum.java
@@ -0,0 +1,24 @@
+public class minPathSum {
+    //最小路径和
+    public int minPathSum(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        for (int i = 0; i <n ; i++) {
+            if(i>0) {
+                grid[0][i]=grid[0][i-1]+grid[0][i];
+            }
+        }
+        for (int i = 0; i <m ; i++) {
+            if(i>0) {
+                grid[i][0]+=grid[i-1][0];
+            }
+        }
+        for (int i = 1; i <m ; i++) {
+            for (int j = 1; j <n ; j++) {
+                grid[i][j]+=Math.min(grid[i-1][j],grid[i][j-1]);
+            }
+        }
+        return grid[m-1][n-1];
+
+    }
+}

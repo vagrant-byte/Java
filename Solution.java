379c67ca2d55685164b1e34107f35cd31f9d1694
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-04-25 20:23:25 +0800

    随机数索引

---
+++ Solution.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
+public class Solution {
+    //随机数索引
+    HashMap<Integer, List<Integer>> map=new HashMap<>();
+    Random random=new Random();
+    public Solution(int[] nums) {
+        for (int i = 0; i <nums.length ; i++) {
+            if(map.containsKey(nums[i])) {
+                map.get(nums[i]).add(i);
+            } else {
+                List<Integer> list=new ArrayList<>();
+                list.add(i);
+                map.put(nums[i],list);
+            }
+        }
+    }
+
+    public int pick(int target) {
+        List<Integer> list=map.get(target);
+        return list.get(random.nextInt(list.size()));
+
+    }
+}

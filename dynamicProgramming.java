647a009ae7ca35176bd7ae84873eb6241030496b
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-05-17 22:17:09 +0800

    动态规划

--- dynamicProgramming.java
+++ dynamicProgramming.java
@@ -1,93 +1,94 @@
-package 剑指offer;
-//动态规划
+package 动态规划;
+
+import java.util.HashMap;
+
 public class dynamicProgramming {
-    //斐波那契数列 青蛙跳台阶
-    public int numWays(int n) {
-        if(n<2) {
-            return 1;
+    //斐波那契数列 递归实现
+    public int fib(int n) {
+        if(n==0||n==1) {
+            return n;
         }
-        int [] num=new int[n+1];
-        num[0]=1;
-        num[1]=1;
-        for(int i=2;i<=n;i++) {
-            num[i]=(num[i-1]+num[i-2])%1000000007;
-        }
-        return num[n];
+        return fib(n-1)+fib(n-2);
     }
-    //股票最大利润
-    public int maxProfit(int[] prices) {
-        if(prices==null||prices.length<=1) {
+    /*针对上述的递归实现复杂度过高,其中一些值进行了重复计算
+    * 因此下面使用剪枝的操作，使用hashmao进行数值的存储避免重复计算*/
+    private HashMap<Integer,Integer> map=new HashMap<>();
+    public int fib1(int n) {
+        if(n==0||n==1) {
+            return n;
+        }
+        int pre=0;//保存n-1的值
+        int ppre=0;//保存n-2的值
+        if(map.containsKey(n-2)) {
+            //hashmap中存在值 就取出
+            ppre=map.get(n-2);
+        } else {
+            //不存在 计算 插入
+            ppre=fib1(n-2);
+            map.put(n-2,ppre);
+        }
+        if(map.containsKey(n-1)) {
+            pre=map.get(n-1);
+        } else {
+            pre=fib1(n-1);
+            map.put(n-1,pre);
+        }
+        return (pre+ppre)%1000000007;
+    }
+    //使用迭代的方法实现
+    public int fib2(int n) {
+        if(n==0) {
             return 0;
         }
-        int res=0;
-        int min=prices[0];
-        for (int i = 1; i <prices.length ; i++) {
-            if(prices[i]<min) {
-                min=prices[i];
-            } else {
-                res=Math.max(res,prices[i]-min);
-            }
+        int first=1;
+        int second=1;
+        int third=1;
+        while (n>2) {
+            third=first+second;
+            first=second;
+            second=third;
+            n--;
         }
-        return res;
+        return third;
     }
-    //连续子数组最大和
-    public int maxSubArray(int[] nums) {
-        int sum=0;
-        int max=Integer.MIN_VALUE;
-        for (int i = 0; i <nums.length ; i++) {
-            if(sum<=0) {
-                sum=nums[i];
-            } else {
-                sum+=nums[i];
-            }
-            max=Math.max(sum,max);
+    //动归实现
+    public int fib3(int n) {
+        if(n==0||n==1) {
+            return n;
         }
-        return max;
-    }
-    //礼物的最大价值
-    public int maxValue(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int []dp = new int[n];
-        for(int i = 0; i < m; i++){
-            dp[0] = dp[0] + grid[i][0];
-            for(int j = 1; j < n; j++){
-                dp[j] = Math.max(dp[j], dp[j-1]) + grid[i][j];
-            }
+        int[] dp=new int[n+1];
+        dp[0]=0;
+        dp[1]=1;
+        for (int i = 2; i <=2 ; i++) {
+            dp[i]=(dp[i-1]+dp[i-2]);
         }
-        return dp[n-1];
+        return dp[n];
     }
-    //把数字翻译成字符串
-    public int translateNum(int num) {
-        if(num<=9) {
+    //矩形覆盖
+    public int rectCover(int target) {
+        if(target<2) {
+            return target;
+        }
+        int[] dp=new int[target+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=target;i++) {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[target];
+    }
+    //青蛙跳台阶
+    public int numWays(int n) {
+        if(n<=1) {
             return 1;
         }
-        int ba=num%100;
-        if(ba<=9||ba>=26) {
-            return translateNum(num/10);
-        } else {
-            return translateNum(num/10)+translateNum(num/100);
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++) {
+            dp[i]=(dp[i-1]+dp[i-2])%1000000007;
         }
-
-    }
-    //最长不含重复字符的子字符串
-    public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0||s.length()==1) {
-            return s.length();
-        }
-        int start=0;
-        int maxLength=0;
-        char[] ch=s.toCharArray();
-        for(int i=1;i<s.length();i++) {
-            for(int j=start;j<i;j++) {
-                if(ch[j]==ch[i]) {
-                    start=j+1;
-                    break;
-                }
-            }
-            maxLength=Math.max(maxLength,i-start+1);
-        }
-        return maxLength;
+        return dp[n];
 
     }
 }

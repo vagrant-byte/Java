21180240239ae049f9b60eb010195627f4911811
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-05-18 21:25:11 +0800

    二叉树

--- TreeNode.java
+++ TreeNode.java
@@ -46,4 +46,52 @@ public class TreeNode {
         }
         return root;
     }
+    //树的子结构
+    public boolean isSubStructure(TreeNode A, TreeNode B) {
+        //空树不是任意一个树的子结构
+        if(A==null||B==null) {
+            return false;
+        }
+        boolean flg=false;
+        if(A.val==B.val) {
+            //判断其是否相同
+            flg=isSameChild(A,B);
+        }
+        if(flg!=true) {
+            //判断左子树是否包含树的子结构
+            flg=isSubStructure(A.left,B);
+        }
+        if(flg!=true) {
+            //判断右子树是否包含树的子结构
+            flg=isSubStructure(A.right,B);
+        }
+        return flg;
+    }
+
+    private boolean isSameChild(TreeNode begin, TreeNode beginSub) {
+        if(beginSub==null) {
+            //说明比较完了
+            return true;
+        }
+        if(begin==null) {
+            //begin为空，说明beginsub不是你的子树
+            return false;
+        }
+        if(begin.val!=beginSub.val) {
+            return false;
+        }
+        return isSameChild(begin.left,beginSub.left)&&isSameChild(begin.right,beginSub.right);
+    }
+    //二叉树镜像
+    public TreeNode mirrorTree(TreeNode root) {
+        if(root==null) {
+            return null;
+        }
+        TreeNode tmp=root.left;
+        root.left=root.right;
+        root.right=tmp;
+        mirrorTree(root.left);
+        mirrorTree(root.right);
+        return root;
+    }
 }

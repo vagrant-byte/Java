caf4f899700f6eb1f8dee104d93d8ff9802d8a96
vagrant-byte <77378428+vagrant-byte@users.noreply.github.com>
2022-09-14 22:40:24 +0800

    Add files via upload

--- 位运算.java
+++ 位运算.java
@@ -1,5 +1,5 @@
 public class 位运算 {
-    //只出现一次的数字
+    //136.只出现一次的数字
     public int singleNumber(int[] nums) {
         int ero=0;
         for(int i=0;i<nums.length;i++) {
@@ -7,7 +7,7 @@ public class 位运算 {
         }
         return ero;
     }
-    //只出现一次的数字II
+    //260.只出现一次的数字II
     public int[] singleNumber1(int[] nums) {
         int[] arr=new int[2];
         int ero=0;
@@ -24,5 +24,23 @@ public class 位运算 {
         arr[0]=ero1;
         arr[1]=ero^ero1;
         return arr;
+    }
+    //保证参数n，不是1就是0
+    //1->0
+    //0->1
+    public int flip(int n) {
+        return n^1;
+    }
+    //n 是非负数 返回1
+    //n 是负数  返回0
+    public int sign(int n) {
+        return flip((n>>31)&1);
+    }
+    public int getMax(int a,int b) {
+        int c=a-b;
+        int scA=sign(c);//a-b非负，scA为1， a-b为负，scA为1
+        int scB=flip(scA);//scA为0，scB为1，scA为1，scB为0
+        //scA为0，scB必为1.scA为1，scB必为0
+        return a*scA+b*scB;
     }
 }
